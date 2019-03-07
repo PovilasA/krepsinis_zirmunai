cc8156021e5b8dec3aeb1e6a00caffa61564b57b
Povilas Asijavicius <povilas.asijavicius@gmail.com>
2019-03-07 17:42:21 +0200

    Create WriteResults class and its tests

---
+++ krepsinis_zirmunai/statistics/write_results.py
@@ -0,0 +1,31 @@
+from krepsinis_zirmunai import google_sheet as gs
+from krepsinis_zirmunai.statistics.individual_summary import *
+
+class WriteResults:
+   def __init__(self, client, spreadsheet_name, worksheet_name, 
+                individual_summary=None, individual_summary_string_range='B3:J15'):
+   
+      self.prepare_worksheet(client, spreadsheet_name, worksheet_name)
+      self.worksheet = gs.Worksheet(client, spreadsheet_name, worksheet_name)
+      self.individual_summary_string_range = individual_summary_string_range
+      self.individual_summary = individual_summary
+
+   def execute(self):
+      if self.individual_summary is not None:
+         values = self.individual_summary.Values().compute()
+         notes = self.individual_summary.Notes().compute()
+         range = self.worksheet.Range(self.individual_summary_string_range)
+         range.change_values(values, headers=False, indices=False)
+         range.change_notes(notes, headers=False, indices=False)
+         range.set_changes()
+      return True
+
+   def prepare_worksheet(self, client, spreadsheet_name, worksheet_name):
+      spreadsheet = client.open(spreadsheet_name)
+      if worksheet_name in [wks.title for wks in spreadsheet.worksheets()]:
+         # make it empty
+         wks = spreadsheet.worksheet_by_title(worksheet_name)
+         wks.clear() # to clear not only values: wks.clear(fields='*')
+      else:
+         # create
+         spreadsheet.add_worksheet(worksheet_name)
---
+++ tests/statistics/test_write_results.py
@@ -0,0 +1,35 @@
+from krepsinis_zirmunai import google_client as gc
+from krepsinis_zirmunai import google_sheet as gs
+
+from krepsinis_zirmunai.statistics.read_input_data import *
+from krepsinis_zirmunai.statistics.individual_summary import *
+from krepsinis_zirmunai.statistics.write_results import *
+
+from tests.helpers.helper_methods import *
+from tests.helpers.mocks import *
+from tests.run_online_tests import *
+
+client = gc.Pygsheets().authenticate().get_client()
+parsed_input = read_mock_table('parsed_input_table.csv', format='dataframe')
+
+def test_execute_with_individual_summary():
+   if RUN_ONLINE_TESTS:
+      create_test_spreadsheet(client, insert_values = False)
+      individual_summary = IndividualSummary(parsed_input)
+      WriteResults(client, 'krepsinis_zirmunai_test_spreadsheet', 'Lankomumas',
+                   individual_summary).execute()
+   
+      expected_values = individual_summary_values()
+      expected_notes = individual_summary_notes()
+      
+      worksheet = gs.Worksheet(client, 'krepsinis_zirmunai_test_spreadsheet',
+                              'Lankomumas')
+      
+      range = worksheet.Range('B3:J15')
+      actual_values = range.get_values_dataframe(headers=True, indices=True)
+      actual_notes = range.get_notes_matrix(headers=False, indices=False)
+      assert actual_values.equals(expected_values)
+      assert actual_notes == expected_notes
+      delete_test_spreadsheet(client)
+
+   #TODO maybe to do offline test?
